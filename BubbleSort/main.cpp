a688fd688408aae32114524cc20c776909e3343f
CrazyLenny <ykxlenny@gmail.com>
2017-08-22 16:12:20 -0400

    Create main file

---
+++ BubbleSort/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Aug 22, 2017
+ *      Author: Kexin
+ */
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "first line for bubble sort!" << endl;
+
+}
+
+
+
